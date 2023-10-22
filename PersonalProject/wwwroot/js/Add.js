fd2601f02f98960b85b58fc04c7947f052018c7f
CameronHigley <Cameron.Higley@My.southeast.edu>
2023-10-21 19:56:04 -0500

    Added dynamic add ingredients to add page

---
+++ PersonalProject/wwwroot/js/Add.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+$(document).ready(() => {
+    let counter = 1;
+    $("#addIngredient").click(() => {
+        $(".ingredients").append($('<br />'));
+        $(".ingredients").append($('<label>Amount<input name="RecipeIngredients[' + counter.toString() + '].Amount" /></label>'));
+        $(".ingredients").append($('<label>Ingredient<input name="Ingredients[' + counter.toString() + '].IngredientName" /></label>'));
+        counter++;
+    });
+});
